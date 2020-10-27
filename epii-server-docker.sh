960c48663a7bafa0fb74401ce1cb622314905df0
MrRen <543169072@qq.com>
2020-10-27 16:30:57 +0800

    init

--- epii-server-docker.sh
+++ epii-server-docker.sh
@@ -52,6 +52,7 @@ function install() {
     ln -s $(pwd)/epii-server-docker.sh /usr/local/bin/esd
     docker run --restart=always --name esc-${version} -p $1:80 -v $a_dir:/epii -itd epii-server:${version} /bin/bash
     start
+    docker exec esc-${version} bash -c "mkdir /epii/logs"
 }
 
 function uninstall() {
